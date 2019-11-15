70364fc580d781080e02311c3325518bbfd03857
brito <luan.tecno@gmail.com>
2019-11-15 13:43:33 -0300

    finishing the refactor

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
-import { router } from "./routes/loginRoutes";
 import { AppRouter } from "./AppRouter";
 
 import "./controllers/LoginController";
+import "./controllers/RootController";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["brito"] }));
-app.use(router);
 app.use(AppRouter.getIntance());
 
 app.listen(3000, () => {
