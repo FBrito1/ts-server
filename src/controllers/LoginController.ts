618162caff5469dafb9007f782fca03c33e34d06
brito <luan.tecno@gmail.com>
2019-11-15 13:29:19 -0300

    add bodyValidator decorators

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { get, controller } from "./decorators";
+import { get, controller, bodyValidator, post } from "./decorators";
 
 @controller("/auth")
 class LoginController {
@@ -18,5 +18,19 @@ class LoginController {
         <button>Submit</button>
       </form>
     `);
+  }
+
+  @post("/login")
+  @bodyValidator("email", "password")
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === "teste@teste.com" && password === "123123") {
+      req.session = { loggedIn: true };
+
+      res.redirect("/");
+    } else {
+      res.send("Invalid email or password");
+    }
   }
 }
